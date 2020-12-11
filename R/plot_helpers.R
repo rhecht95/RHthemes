5c9dca4cefa4a3d0888e58ee01fcf7d9fee0e126
Ryan Hecht <ryan.hecht@me.com>
2020-12-10 22:41:25 -0800

    update NAMESPACE

--- R/plot_helpers.R
+++ R/plot_helpers.R
@@ -1,3 +1,17 @@
 #plot helpers: these objects add aditional customization to ggplots beyond theming
 
-neat_chart <- list(scale_y_continuous(expand = expansion(mult = c(0, 0.1))))
+#' neat_chart
+#'
+#' a function that cleans up plot appearance by closing the axis gap
+#' @return none
+#' @export
+#' @importFrom ggplot2 scale_y_continuous
+#' @examples
+#' a_plot + neat_chart()
+neat_chart <- function() {
+
+
+  list(ggplot2::scale_y_continuous(expand = expansion(mult = c(0, 0.1))))
+
+
+}
