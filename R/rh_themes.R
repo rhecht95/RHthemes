5834917d3581a4a73f6bfa39019314dd4ac1f2f3
Ryan Hecht <ryan.hecht@me.com>
2021-02-15 22:34:59 -0800

    reposition subtitle 5

--- R/rh_themes.R
+++ R/rh_themes.R
@@ -17,9 +17,9 @@ bw_RH <- function() {
                axis.ticks.length.y = unit(0.25, "cm"),
                axis.title.y = element_text(margin = margin(l = 7, r = 10), family = "HelveticaNeue-Bold"),
                axis.title.x = element_text(family = "HelveticaNeue-Bold"),
-               plot.title = element_text(size = 16, family = "HelveticaNeue-Bold", vjust = 2),
-               plot.subtitle = element_text(size = 13, family = "HelveticaNeue-BoldItalic", hjust = -2),
-               plot.caption = element_text(hjust = 0),
+               plot.title = element_text(size = 16, family = "HelveticaNeue-Bold", vjust = 2, hjust = .5),
+               plot.subtitle = element_text(size = 12, family = "HelveticaNeue-BoldItalic", hjust = .5),
+               plot.caption = element_text(hjust = .5),
                text= element_text(family = "HelveticaNeue"),
                legend.position = c(0.9, 0.8))
 
