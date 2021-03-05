e7abcc2f626c6ce303dd4e8947e622ff88f62f52
Ryan Hecht <ryan.hecht@me.com>
2021-03-04 21:47:59 -0800

    pastels2 palette

--- R/rh_colors.R
+++ R/rh_colors.R
@@ -28,3 +28,26 @@ pastels1 <- function(...) {
 
 }
 
+pastels2 <- function(...) {
+  pastels2_vec <- c(
+
+    'dark plum' = "#4e3d42",
+    'forrest green' =  "#4e6e58",
+    'periwinkle' = "#a4a8d1",
+    'light coffee' = "#b38a58",
+    'pastel orange' = "#f78154",
+    'pale green' = "#D2E4C4",
+    'deep blue' =  "#172A3A",
+    'lavender' = "#AA78A6",
+    'dandelion' = "#FFD97D"
+
+  )
+
+  selected_colors <- c(...)
+
+  if (is.null(selected_colors))
+    return (pastel1_vec)
+
+  pastel1_vec[selected_colors]
+
+}
